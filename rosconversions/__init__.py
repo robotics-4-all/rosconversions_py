c84cadce714cdb298e2e65fc9f759583b37e8549
Konstantinos Panayiotou <klpanagi@gmail.com>
2018-04-14 22:47:02 +0300

    Export get_service_class

--- rosconversions/__init__.py
+++ rosconversions/__init__.py
@@ -1,3 +1,5 @@
-from conv import dict_to_ros_msg, ros_msg_to_dict, get_message_class, dict_to_ros_srv_request, dict_to_ros_srv_response, ros_srv_req_to_dict, ros_srv_resp_to_dict
+from conv import dict_to_ros_msg, ros_msg_to_dict, get_message_class, dict_to_ros_srv_request, dict_to_ros_srv_response, ros_srv_req_to_dict, ros_srv_resp_to_dict, get_service_class
 
-__all__ = ["dict_to_ros_msg", "ros_msg_to_dict", "get_message_class", "dict_to_ros_srv_request", "dict_to_ros_srv_response", "ros_srv_req_to_dict", "ros_srv_resp_to_dict"]
+__all__ = ["dict_to_ros_msg", "ros_msg_to_dict", "get_message_class", "dict_to_ros_srv_request",
+           "dict_to_ros_srv_response", "ros_srv_req_to_dict", "ros_srv_resp_to_dict",
+           "get_service_class"]
