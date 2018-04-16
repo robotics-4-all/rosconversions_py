bf304466346e73a3e746ba475e397a2c2041cb2f
Konstantinos Panayiotou <klpanagi@gmail.com>
2018-04-17 01:16:41 +0300

    Test for fill_ros_message()

--- test/test_msgs.py
+++ test/test_msgs.py
@@ -5,7 +5,8 @@ from __future__ import print_function
 import unittest
 import time
 
-from rosconversions import dict_to_ros, ros_to_dict, get_message_class
+from rosconversions import (get_message_class,
+                            fill_ros_message)
 
 class TestROSMsgTest(unittest.TestCase):
     def setUp(self):
@@ -20,6 +21,16 @@ class TestROSMsgTest(unittest.TestCase):
         msg_cls = get_message_class('sensor_msgs/Image')
         msg = msg_cls()
         self.assertIsInstance(msg, Image)
+
+    def test_fill_message(self):
+        from std_msgs.msg import String
+        d = {
+            'data': "Filled"
+        }
+        msg = String()
+        fill_ros_message(msg, d)
+        self.assertIsInstance(msg, String, msg=None)
+        self.assertEqual(d['data'], msg.data)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
